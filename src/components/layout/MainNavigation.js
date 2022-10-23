bd41e5c0a63e109e46b4bcb2abaf17208e3ba43c
vitamin777 <rlaalsdn8@gmail.com>
2022-10-23 23:59:34 +0900

    style : navigation 스타일링
    -MainNavigation.module.css file 추가하여 네비게이션에 필요한 css 설정
    -MainNavigation.js 에서 classes 로 import 하여 동적으로 속성을 가져와 css 적용
    
    -출력 확인 커밋

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,11 +1,17 @@
-/*네비게이션 관련 주요 컴포넌트들이 정의될 컴포넌트*/
+/*!네비게이션 관련 주요 컴포넌트들이 정의될 컴포넌트!*/
+
+/*표준 js 에서는 동작하지않지만 별도의 빌드 과정에 코드가 브라우저에 도착하기전에 해석하고 변환해주기때문에
+* css file 부터 import 할 수 있다, classes 는 css file 에서 정의한 모든 css 클래스들이 classes 객체의 속성이된다
+* 이 객체를 JSX 코드에서 사용해서 클래스를 element 에 추가하면 내부적으로 코드가 변환되면서
+클래스들의 이름이 각 컴포넌트에 대해 유일하도록 바뀜*/
+import classes from './MainNavigation.module.css';
 
 import {Link} from "react-router-dom";
 
 const MainNavigation = () => {
   return (
-    <header>
-      <div>모임 사이트</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>모임 동호회</div>
       <nav>
         <ul>
           <li>
