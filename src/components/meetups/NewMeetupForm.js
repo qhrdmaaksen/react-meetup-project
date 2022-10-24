d95690a476b89c230670e70995f5d71749ad5df8
vitamin777 <rlaalsdn8@gmail.com>
2022-10-25 02:19:53 +0900

    feat : 신규 모임 추가 시 firebase data 등록
    -useRef 를 활용해 입력 받은 현재 값을 담아 fetch 로 요청 보낼 url 설정 후 post 로 보냈으며 성공 및 실패에 대해서도 로직 작성하였음
    -firebase 에 데이터가 담긴 객체들이 등록되었음을 확인하였음
    
    -기능 정상 확인 커밋

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,26 +2,62 @@
 
 import classes from './NewMeetupForm.module.css';
 import Card from '../ui/Card';
+import {useRef, useState} from "react";
 
 const NewMeetupForm = () => {
+  const titleInputRef = useRef()
+  const imageInputRef = useRef()
+  const addressInputRef = useRef()
+  const descriptionInputRef = useRef()
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    fetch('https://react-meetup-project-df2fd-default-rtdb.firebaseio.com/meetups.json'
+        , {
+      method: 'POST',
+          body: JSON.stringify({
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+          })
+        }).then(response => {
+          if(response.ok){
+            console.log('신규 모임 db 등록 성공')
+            return response.json()
+          } else {
+            return response.json().then(data => {
+              console.log(data)
+              let errorMessage = '모임 등록 실패'
+              throw new Error(errorMessage)
+            })
+          }
+    })
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">모임 이름</label>
-          <input type="text" id="title" />
+          <input type="text" id="title" ref={titleInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="image">모임 이미지</label>
-          <input type="text" id="image" />
+          <input type="text" id="image" ref={imageInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="address">모임 장소</label>
-          <input type="text" id="address" />
+          <input type="text" id="address" ref={addressInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="description">모임 설명</label>
-          <textarea id="description"></textarea>
+          <textarea id="description" ref={descriptionInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>모임 추가</button>
