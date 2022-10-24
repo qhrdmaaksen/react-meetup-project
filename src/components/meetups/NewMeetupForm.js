8732a976f604e309a45d264741f66359cdcf9581
vitamin777 <rlaalsdn8@gmail.com>
2022-10-25 04:01:02 +0900

    refactor : 요청 api 를 NewMeetup.js 에서 작성토록 바꿈
    -NewMeetupForm.js 에 코드량이 많아 신규 모임 데이터를 props.onAddMeetup 으로 전달하여 NewMeetup.js 에서 프롭으로 전달받은 데이터를 함수에서 인수로 받아
    api 요청 작성 후 인수로 받은 데이터를 바디에 넣어 신규 모임 데이터를 파이어베이스에 등록토록 함
    -NewMeetupForm.js 에서 jsx 에 인풋 이미지에 타입을 url 로 받을 것이기때문에 url 로 변경하였으며 각 입력 받을곳은 required 로 속성 추가하여 필수로 입력 받도록함
    ,button type 도 submit 으로 명시하였으며 textarea 도 기본 5 행으로 설정
    
    -기능 정상 확인 커밋

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,65 +2,64 @@
 
 import classes from './NewMeetupForm.module.css';
 import Card from '../ui/Card';
-import {useRef, useState} from "react";
+import { useRef, useState } from 'react';
 
-const NewMeetupForm = () => {
-  const titleInputRef = useRef()
-  const imageInputRef = useRef()
-  const addressInputRef = useRef()
-  const descriptionInputRef = useRef()
+const NewMeetupForm = (props) => {
+  /*useRef 로 입력 받을 input 에 대한 참조 객체들*/
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
   const submitHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    /*useRef 로 생성된 모든 ref 객체는 current 속성을 가지고있으며 이에 연결된 값이있다.
+     * 만약 화면에 출력되는것을 임의로 바꾸고싶다면 useState 를 사용해야한다.*/
     const enteredTitle = titleInputRef.current.value;
     const enteredImage = imageInputRef.current.value;
     const enteredAddress = addressInputRef.current.value;
     const enteredDescription = descriptionInputRef.current.value;
 
-    fetch('https://react-meetup-project-df2fd-default-rtdb.firebaseio.com/meetups.json'
-        , {
-      method: 'POST',
-          body: JSON.stringify({
-            title: enteredTitle,
-            image: enteredImage,
-            address: enteredAddress,
-            description: enteredDescription,
-          })
-        }).then(response => {
-          if(response.ok){
-            console.log('신규 모임 db 등록 성공')
-            return response.json()
-          } else {
-            return response.json().then(data => {
-              console.log(data)
-              let errorMessage = '모임 등록 실패'
-              throw new Error(errorMessage)
-            })
-          }
-    })
-  }
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    /*사용자 입력값이 잘 들어오는지 db 에 보내기전 콘솔로그로 확인하는 습관을 갖자*/
+    console.log('신규 모임 양식 데이터 :::', meetupData);
+
+    /*부모 컴포넌트로 입력값 전달*/
+    props.onAddMeetup(meetupData);
+  };
 
   return (
     <Card>
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">모임 이름</label>
-          <input type="text" id="title" ref={titleInputRef}/>
+          <input type="text" id="title" ref={titleInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">모임 이미지</label>
-          <input type="text" id="image" ref={imageInputRef}/>
+          <input type="url" id="image" ref={imageInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">모임 장소</label>
-          <input type="text" id="address" ref={addressInputRef}/>
+          <input type="text" id="address" ref={addressInputRef} required />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">모임 설명</label>
-          <textarea id="description" ref={descriptionInputRef}></textarea>
+          <textarea
+            id="description"
+            ref={descriptionInputRef}
+            required
+            rows="5"
+          ></textarea>
         </div>
         <div className={classes.actions}>
-          <button>모임 추가</button>
+          <button type="submit">모임 추가</button>
         </div>
       </form>
     </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,10 +3,37 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+        'https://react-meetup-project-df2fd-default-rtdb.firebaseio.com/meetups.json',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            meetupData,
+          }),
+          headers: {
+            'Content-Type': 'applications/json',
+          }
+        },
+    ).then((response) => {
+      if (response.ok) {
+        console.log('신규 모임 db 등록 성공');
+        return response.json();
+      } else {
+        return response.json().then((data) => {
+          console.log(data);
+          let errorMessage = '모임 등록 실패';
+          throw new Error(errorMessage);
+        });
+      }
+    });
+  }
+
   return (
     <div>
       <h3>신규 모임 추가</h3>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </div>
   );
 };
