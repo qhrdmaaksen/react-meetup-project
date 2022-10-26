4a992bcd2137aff1029a7da4ef862d1ef541233d
vitamin777 <rlaalsdn8@gmail.com>
2022-10-26 19:18:50 +0900

    feat : 즐겨찾기 기능 추가
    -MeetupItem.js 에서 useContext 로 즐겨찾기에 사용될 상태들을 관리할 store 인 FavoritesContext 에 접근하는 상수 itemIsFavorite 로 즐겨 찾기 버튼을 클릭 시
    즐겨찾기가 되어있다면 store 에 작성한 removeFavorite 함수로 해당 아이템에 대한 아이디로 제거 할테고 즐겨찾기가 되어있지않다면 store 에 작성한 addFavorite 함수로 해당 아이템에 대한 정보를 props 로 전달 받아 추가토록 함
    -즐겨찾기가되어있다면 즐겨찾기 해제, 안되어있다면 즐겨찾기 추가로 변경토록 함
     -Favorites.js 에서 useContext 로 store 에 접근할 favoritesCtx 상수를 작성하여
     favoritesCtx 로 totalFavorites (즐겨찾기 등록되어있는 갯수) 가 0 이면 content 변수에 즐겨찾기 목록없다 등록하라 라는 메시지 출력되도록하였으며 등록된 즐겨찾기가 있다면 즐겨찾기된 모임 목록을 MeetupList component 로 보여주게하였으며 해당 목록의 아이템은 store 로 접근하여 favorites 가 갖는 배열에담긴 즉 즐겨찾기가 등록된 아이템을 보여주도록함
    
     -기능 정상 작동 및 출력 확인

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,23 +1,45 @@
 /*모임 목록의 개별 아이템 컴포넌트*/
 
 import classes from './MeetupItem.module.css';
-import Card from "../ui/Card";
+import Card from '../ui/Card';
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
 
 const MeetupItem = (props) => {
+  const favoriteCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoriteCtx.itemIsFavorite(props.id);
+
+  const toggleFavoriteStatusHandler = () => {
+    if (itemIsFavorite) {
+      favoriteCtx.removeFavorite(props.id);
+    } else {
+      favoriteCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        address: props.address,
+        description: props.description,
+        image: props.image,
+      });
+    }
+  };
+
   return (
     <li className={classes.item}>
       <Card>
-      <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>즐겨찾기 추가</button>
-      </div>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? '즐겨찾기 해제' : '즐겨찾기 추가'}
+          </button>
+        </div>
       </Card>
     </li>
   );
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,25 @@
 /*즐겨찾기 등록된 모임들을 볼 수 있는 컴포넌트*/
+
+import { useContext } from 'react';
+import FavoritesContext from './../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 const FavoritesPage = () => {
-	return (
-			<div>즐겨 찾기 페이지</div>
-	)
-}
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>즐겨찾기에 등록된 모임이 없습니다. 즐겨찾기 추가해주세요.</p>;
+		console.log(content)
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <div>
+      <h2>나의 즐겨 찾기 목록</h2>
+      {content}
+    </div>
+  );
+};
 export default FavoritesPage;
